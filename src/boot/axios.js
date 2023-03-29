a5e8900d7dec9286c057e8396f1c518c68e332e0
kolodiva <kolodiva@mail.ru>
2023-03-29 12:56:57 +0300

    ok

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://kolodiva.com', headers: {Origin: 'http://localhost:4000', Referer: 'http://localhost:4000'} })
+const api = axios.create({ baseURL: 'localhost:3000'})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
