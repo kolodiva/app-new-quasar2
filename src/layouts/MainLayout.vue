68f138780713135562a028fa0afc480a800bfc37
kolodiva <kolodiva@mail.ru>
2023-03-29 14:38:12 +0300

    ok

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,141 +1,5 @@
 <template>
-  <div class="q-pa-md q-gutter-sm">
-    <q-btn label="Carousel" color="primary" @click="carousel = true" />
-    <q-btn label="Card" color="primary" @click="card = true" />
-    <q-btn label="Sliders" color="primary" @click="sliders = true" />
-
-    <q-dialog v-model="carousel">
-      <q-carousel
-        transition-prev="slide-right"
-        transition-next="slide-left"
-        swipeable
-        animated
-        :autoplay="autoplay"
-        infinite
-        v-model="slide"
-        control-color="primary"
-        navigation-icon="flutter_dash"
-        navigation
-        padding
-        height="600px"
-        class="bg-white shadow-1 rounded-borders"
-        arrows
-        @mouseenter="autoplay = false"
-        @mouseleave="autoplay = true"
-        style="overflow-x: hidden"
-
-      >
-        <q-carousel-slide :name="1" class="column no-wrap flex-center">
-          <q-icon name="style" color="primary" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="2" class="column no-wrap flex-center">
-          <q-icon name="live_tv" color="primary" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="3" class="column no-wrap flex-center">
-          <q-icon name="layers" color="primary" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="4" class="column no-wrap flex-center">
-          <q-icon name="terrain" color="primary" size="56px" />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-      </q-carousel>
-    </q-dialog>
-
-    <q-dialog v-model="card">
-      <q-card class="my-card">
-        <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
-
-        <q-card-section>
-          <q-btn
-            fab
-            color="primary"
-            icon="place"
-            class="absolute"
-            style="top: 0; right: 12px; transform: translateY(-50%);"
-          />
-
-          <div class="row no-wrap items-center">
-            <div class="col text-h6 ellipsis">
-              Cafe Basilico
-            </div>
-            <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
-              <q-icon name="place" />
-              250 ft
-            </div>
-          </div>
-
-          <q-rating v-model="stars" :max="5" size="32px" />
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">
-            $・Italian, Cafe
-          </div>
-          <div class="text-caption text-grey">
-            Small plates, salads & sandwiches in an intimate setting.
-          </div>
-        </q-card-section>
-
-        <q-separator />
-
-        <q-card-actions align="right">
-          <q-btn v-close-popup flat color="primary" label="Reserve" />
-          <q-btn v-close-popup flat color="primary" round icon="event" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="sliders">
-      <q-card style="width: 300px" class="q-px-sm q-pb-md">
-        <q-card-section>
-          <div class="text-h6">Volumes</div>
-        </q-card-section>
-
-        <q-item-label header>Media volume</q-item-label>
-        <q-item dense>
-          <q-item-section avatar>
-            <q-icon name="volume_up" />
-          </q-item-section>
-          <q-item-section>
-            <q-slider color="teal" v-model="slideVol" :step="0" />
-          </q-item-section>
-        </q-item>
-
-        <q-item-label header>Alarm volume</q-item-label>
-        <q-item dense>
-          <q-item-section avatar>
-            <q-icon name="alarm" />
-          </q-item-section>
-          <q-item-section>
-            <q-slider color="teal" v-model="slideAlarm" :step="0" />
-          </q-item-section>
-        </q-item>
-
-        <q-item-label header>Ring volume</q-item-label>
-        <q-item dense>
-          <q-item-section avatar>
-            <q-icon name="vibration" />
-          </q-item-section>
-          <q-item-section>
-            <q-slider color="teal" v-model="slideVibration" :step="0" />
-          </q-item-section>
-        </q-item>
-      </q-card>
-    </q-dialog>
-
     <div>{{guid}}</div>
-  </div>
 </template>
 
 
@@ -168,7 +32,7 @@ export default {
             //   messageColor: 'black'
             // });
 
-    api.get('/test?id=2356')
+    axios.get('https://kolodiva.com/test?id=2356')
       .then((response) => {
         //guid.value = response.data.guid
         myStore.increment(response.data.guid);
@@ -190,7 +54,7 @@ export default {
 
 <script setup>
 import { ref } from 'vue'
-import { api} from 'boot/axios'
+import axios from 'axios'
 import { useMyStore } from 'stores/myStore'
 import { computed } from 'vue';
 
