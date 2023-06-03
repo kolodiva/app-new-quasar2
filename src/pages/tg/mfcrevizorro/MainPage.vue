fedbb6455d906f9a7cde9a30f86ad7581aa309bc
kolodiva <kolodiva@mail.ru>
2023-06-03 12:07:11 +0300

    ok

--- src/pages/tg/mfcrevizorro/MainPage.vue
+++ src/pages/tg/mfcrevizorro/MainPage.vue
@@ -4,13 +4,32 @@
       <div v-for="n in orders" :key='n.order_guid'>
           <q-expansion-item group="somegroup"
           class="text-h5 shadow-1 overflow-hidden q-my-md" :label="`${n.order_id}`" header-class="bg-primary text-white" expand-icon-class="text-white"
-          @show="startCounting(n.order_guid)" style="border-radius: 20px">
+          @show="getOrderPositions(n.order_guid)" style="border-radius: 20px">
             <q-card class="">
               <q-card-section>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-              </q-card-section>
+                <q-tree
+                      :nodes="simple"
+                      accordion
+                      node-key="label"
+                      dense
+                      v-model:expanded="expanded"
+                    >
+
+                    <template v-slot:default-header="prop">
+                            <div class="row items-center">
+                              <div class="text-h6  text-primary">{{ prop.node.label }}</div>
+                            </div>
+                          </template>
+
+                          <template v-slot:default-body="prop">
+                            <div v-if="prop.node.story" class="text-subtitle1" >
+                              {{ prop.node.story }}
+                            </div>
+                            <!-- <span v-else class="text-weight-light text-black">This is some default content.</span> -->
+                          </template>
+
+                  </q-tree>
+                </q-card-section>
             </q-card>
           </q-expansion-item>
 
@@ -24,15 +43,37 @@
 
 <script setup>
 
+  import { postQueryTG } from 'boot/axios'
+
+  import { ref, computed } from 'vue';
+
   import { useTGStore } from 'stores/storeTG'
   import { storeToRefs } from 'pinia'
+
 
   const storeTG = useTGStore();
   const {orders} = storeToRefs(storeTG);
 
-  const startCounting = (order_guid) => {
+  const simple = ref([])
+  const expanded = ref([ 'Заказ однако' ])
+
+  const getOrderPositions = async (order_guid) => {
     //console.log(order_guid)
     //здесь иы делаем Запрос на вывод позиций Заказа с кол-вом и собранными товарами
-  }
+    const res = await postQueryTG({oper:'getOrderPositions', order_guid})
+
+  //console.log(res.resp)
+
+    if (res.resp) {
+      simple.value = [{label: 'Заказ однако', header: 'root', children: res.resp.positions}];
+    } else {
+      $q.notify({
+        color: 'negative',
+        position: 'top',
+        message: `Loading failed ${res.error}`,
+        icon: 'report_problem'
+      })
+    }
+}
 
 </script>
